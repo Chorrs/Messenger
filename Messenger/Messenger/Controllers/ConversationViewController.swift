7056040f52c2201b212b122737d82924bb782c2b
Chorrs <Pechorskaya.K8@gmail.com>
2024-01-23 14:19:18 +0200

    wrap up getting started

---
+++ Messenger/Messenger/Controllers/ConversationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Chorrs on 23.01.24.
+//
+
+import UIKit
+
+class ConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
