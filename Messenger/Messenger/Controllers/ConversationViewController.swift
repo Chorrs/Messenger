0d6d6cde5a6892aa78f64dcead7c70119b57a97f
Chorrs <Pechorskaya.K8@gmail.com>
2024-01-23 22:43:17 +0200

    1) Necessary ViewControllers have been added. 2) Check has been added: if the user is signed in based on user defaults and if they are we'll stay on the Conversation screen and if they're not will show the logIn screen.

--- Messenger/Messenger/Controllers/ConversationViewController.swift
+++ Messenger/Messenger/Controllers/ConversationViewController.swift
@@ -11,7 +11,20 @@ class ConversationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
         
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn {
+            let vc = LogInViewController()
+            let navVC = UINavigationController(rootViewController: vc)
+            navVC.modalPresentationStyle = .fullScreen
+            present(navVC, animated: false)
+        }
     }
 
 
---
+++ Messenger/Messenger/Controllers/LogIn/LogInViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LogInViewController.swift
+//  Messenger
+//
+//  Created by Chorrs on 23.01.24.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Log In"
+        view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Register",
+            style: .done,
+            target: self,
+            action: #selector(didTapRegister)
+        )
+    }
+    
+    @objc private func didTapRegister() {
+        let registerVC = RegisterViewController()
+        registerVC.title = "Create Account"
+        navigationController?.pushViewController(registerVC, animated: true)
+    }
+    
+}
