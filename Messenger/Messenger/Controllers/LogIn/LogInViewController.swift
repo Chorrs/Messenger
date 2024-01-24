33608cec4426e44324686d5f34448a7076fb2bd2
Chorrs <Pechorskaya.K8@gmail.com>
2024-01-24 23:16:14 +0200

    Configured login view controller with the addition of a logo, input fields for email and password, and a registration button.

--- Messenger/Messenger/Controllers/LogIn/LogInViewController.swift
+++ Messenger/Messenger/Controllers/LogIn/LogInViewController.swift
@@ -9,17 +9,113 @@ import UIKit
 
 class LogInViewController: UIViewController {
     
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private  let emailField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Email Address..."
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        return field
+    }()
+    
+    private  let passwordField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .done
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Password..."
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let loginButtonL: UIButton = {
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0, green: 0.637983311, blue: 1, alpha: 1)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Log In"
         view.backgroundColor = .white
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(
+        let registerButton = UIBarButtonItem(
             title: "Register",
             style: .done,
             target: self,
             action: #selector(didTapRegister)
         )
+        registerButton.tintColor = #colorLiteral(red: 0, green: 0.637983311, blue: 1, alpha: 1)
+        navigationItem.rightBarButtonItem = registerButton
+        
+        
+    // MARK: Add subviews
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(passwordField)
+        scrollView.addSubview(loginButtonL)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.frame = view.bounds
+        let size = scrollView.width/3
+        imageView.frame = CGRect(
+            x: (scrollView.width - size)/2,
+            y: 20,
+            width: size,
+            height: size
+        )
+        emailField.frame = CGRect(
+            x: 30,
+            y: imageView.bottom+10,
+            width: scrollView.width-60,
+            height: 52
+        )
+        passwordField.frame = CGRect(
+            x: 30,
+            y: emailField.bottom+10,
+            width: scrollView.width-60,
+            height: 52
+        )
+        loginButtonL.frame = CGRect(
+            x: 30,
+            y: passwordField.bottom+10,
+            width: scrollView.width-60,
+            height: 52
+        )
+        
     }
     
     @objc private func didTapRegister() {
